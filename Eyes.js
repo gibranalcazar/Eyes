28a5c579041dd2896c2b368a0ada5c4d478ad9ca
GN-AM <gibran@live.com>
2022-05-05 21:12:44 -0500

    S4

--- Eyes.js
+++ Eyes.js
@@ -1,5 +1,6 @@
 var balls = document.getElementsByClassName("ball");
-
+var bodytag = document.getElementById("bodytag");
+var colorInput = document.getElementById("colorInput").value;
 document.onmousemove = (event) => {
   var x = (event.clientX * 100) / window.innerWidth + "%";
   var y = (event.clientY * 100) / window.innerHeight + "%";
@@ -11,4 +12,10 @@ document.onmousemove = (event) => {
     console.log(x,y);
   }
 
+};
+
+function selectColor(){
+  colorInput = document.getElementById("colorInput").value;
+  console.log(colorInput);
+  bodytag.style.background = colorInput;
 };
